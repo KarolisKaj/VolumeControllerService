f0c314d6c18ebf435a9dc1c4c07e2861068761d8
KarolisKaj <karolis.kajenas1@gmail.com>
2017-09-09 19:55:11 +0300

    Adds UPD firewall rule.

--- VolumeControllerService/Utility/FireWall.cs
+++ VolumeControllerService/Utility/FireWall.cs
@@ -19,19 +19,23 @@
                     return;
             }
 
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(CreateRule(currentProfiles, 6, CommuncationDetails.Port));
+            firewallPolicy.Rules.Add(CreateRule(currentProfiles, 17, CommuncationDetails.PortUDP.ToString()));
+        }
+
+        private static INetFwRule2 CreateRule(int currentProfiles, int protocol, string port)
+        {
             INetFwRule2 inboundRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
             inboundRule.Enabled = true;
             inboundRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
-            inboundRule.Protocol = 6;
-            inboundRule.LocalPorts = CommuncationDetails.Port;
+            inboundRule.Protocol = protocol;
+            inboundRule.LocalPorts = port;
             inboundRule.Name = ApplicationData.ServiceName;
             inboundRule.serviceName = "*"; // For all services. Services only.
 
             inboundRule.Profiles = currentProfiles;
-
-            // Now add the rule
-            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
-            firewallPolicy.Rules.Add(inboundRule);
+            return inboundRule;
         }
     }
 }
